c54cb51cdd9aed55687b0120ba400e84be47304a
Matt Enyeart <menyeart1@gmail.com>
2024-02-01 11:15:52 -0700

    Complete iteration 3

--- practice_independent_challenges/week5/reunion/lib/reunion.rb
+++ practice_independent_challenges/week5/reunion/lib/reunion.rb
@@ -11,4 +11,24 @@ class Reunion
     @activities << activity
   end
 
+  def total_cost
+    @activities.sum { |activity| activity.total_cost }
+  end
+
+  def cost_breakdown
+    owed = Hash.new(0)
+    @activities.each do |activity|
+      activity.owed.each do |name, cost|
+        owed[name] += cost
+      end
+    end
+    owed
+  end
+
+  def print_summary
+    cost_breakdown.each do |name, cost|
+      print "#{name} owes $#{cost}\n"
+    end
+  end
+
 end

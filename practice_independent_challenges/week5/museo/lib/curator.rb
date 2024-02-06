1f3895582a6e0113c8e91e4a10533074d8ef1e82
Matt Enyeart <menyeart1@gmail.com>
2024-02-06 09:49:39 -0700

    Complete iteration 3

--- practice_independent_challenges/week5/museo/lib/curator.rb
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -18,4 +18,31 @@ class Curator
   def find_artist_by_id(id)
     @artists.find { |artist| artist.id == id}
   end
+
+  def all_artists_and_photos
+    @artists.map { |artist| {artist.name => get_artists_photos(artist) } }
+  end
+
+  def get_artists_photos(artist)
+    @photographs.find_all { |photo| photo.artist_id == artist.id}.map { |photo| photo.name}
+  end
+
+  def artists_with_mult_photos
+    all_artists_and_photos.map do |art_photos| 
+      art_photos.keys[0] if art_photos.values.flatten.count > 1
+    end.compact
+  end
+
+  def photos_by_artist_origin(origin)
+    photos = []
+    artist_ids = @artists.map { |artist| artist.id if artist.country == origin}.compact
+    artist_ids.each do |id|
+      @photographs.each do |photo|
+        if photo.artist_id == id
+          photos << photo.name
+        end
+      end
+    end
+  photos
+  end
 end
--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -90,4 +90,162 @@ RSpec.describe "Curator" do
     expect(curator.find_artist_by_id("1")).to eq(artist_1)
     expect(curator.find_artist_by_id("2")).to eq(artist_2)
   end
+
+  it "can return a list of all artists and their photographs" do
+    curator = Curator.new
+
+    artist_1 = Artist.new({
+      id: "1",      
+      name: "Henri Cartier-Bresson",      
+      born: "1908",      
+      died: "2004",      
+      country: "France"      
+    })     
+
+    artist_2 = Artist.new({
+      id: "2",      
+      name: "Ansel Adams",      
+      born: "1902",      
+      died: "1984",      
+      country: "United States"      
+    })   
+  
+    photo_1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    })
+    
+    photo_2 = Photograph.new({
+      id: "2",
+      name: "Monolith the Northwest Face of Half Dome",
+      artist_id: "2",
+      year: "1956"
+    })  
+
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+
+    expect(curator.all_artists_and_photos).to eq([{"Henri Cartier-Bresson" => ["Rue Mouffetard, Paris (Boy with Bottles)"]}, {"Ansel Adams" => ["Monolith the Northwest Face of Half Dome"]}])
+  end
+
+  it "can return a list of all artists and their photographs" do
+    curator = Curator.new
+
+    artist_1 = Artist.new({
+      id: "1",      
+      name: "Henri Cartier-Bresson",      
+      born: "1908",      
+      died: "2004",      
+      country: "France"      
+    })     
+
+    artist_2 = Artist.new({
+      id: "2",      
+      name: "Ansel Adams",      
+      born: "1902",      
+      died: "1984",      
+      country: "United States"      
+    })   
+  
+    photo_1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    })
+    
+    photo_2 = Photograph.new({
+      id: "2",
+      name: "Monolith the Northwest Face of Half Dome",
+      artist_id: "2",
+      year: "1956"
+    })  
+
+    photo_3 = Photograph.new({
+      id: "3",
+      name: "Guy who got shot",
+      artist_id: "1",
+      year: "1945"
+    })
+
+    photo_4 = Photograph.new({
+      id: "4",
+      name: "That horse in the sierra sun",
+      artist_id: "2",
+      year: "1945"
+    })
+
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+
+    expect(curator.artists_with_mult_photos).to eq([artist_1.name])
+
+    curator.add_photograph(photo_4)
+
+    expect(curator.artists_with_mult_photos).to eq([artist_1.name, artist_2.name])
+  end
+
+  it "can return a list of `Photograph`s that were taken by a photographer from that country" do
+    curator = Curator.new
+
+    artist_1 = Artist.new({
+      id: "1",      
+      name: "Henri Cartier-Bresson",      
+      born: "1908",      
+      died: "2004",      
+      country: "France"      
+    })     
+
+    artist_2 = Artist.new({
+      id: "2",      
+      name: "Ansel Adams",      
+      born: "1902",      
+      died: "1984",      
+      country: "United States"      
+    })   
+  
+    photo_1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    })
+    
+    photo_2 = Photograph.new({
+      id: "2",
+      name: "Monolith the Northwest Face of Half Dome",
+      artist_id: "2",
+      year: "1956"
+    })  
+
+    photo_3 = Photograph.new({
+      id: "3",
+      name: "Guy who got shot",
+      artist_id: "1",
+      year: "1945"
+    })
+
+    photo_4 = Photograph.new({
+      id: "4",
+      name: "That horse in the sierra sun",
+      artist_id: "2",
+      year: "1945"
+    })
+
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+
+    expect(curator.photos_by_artist_origin("France")).to eq([photo_1.name, photo_3.name])
+  end
 end
